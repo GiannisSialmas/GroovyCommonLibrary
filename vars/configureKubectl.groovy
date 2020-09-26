bc6575dba84f921b12d9d95df0a9d7dda0d69ed5
Giannis Sialmas <giannissialmas@gmail.com>
2020-09-26 20:14:26 +0300

    Update configureKubectl.groovy

--- vars/configureKubectl.groovy
+++ vars/configureKubectl.groovy
@@ -5,13 +5,13 @@
 // Another way to do this
 
 def call(Map args) {
-
-			sh "kubectl config set clusters.default.server ${args.clusterIp}";
-			sh "kubectl config set clusters.default.insecure-skip-tls-verify true";
-			sh "kubectl config set users.default.token ${args.clusterToken}";
-			sh "kubectl config set contexts.default.cluster default";
-			sh "kubectl config set contexts.default.user default";
-			sh "kubectl config use-context default";
-			sh "kubectl config set-context --current --namespace=${args.namespace}"
+	
+	sh "kubectl config set clusters.default.server ${args.clusterIp}";
+	sh "kubectl config set clusters.default.insecure-skip-tls-verify true";
+	sh "kubectl config set users.default.token ${args.clusterToken}";
+	sh "kubectl config set contexts.default.cluster default";
+	sh "kubectl config set contexts.default.user default";
+	sh "kubectl config use-context default";
+	sh "kubectl config set-context --current --namespace=${args.namespace}"
 
 }

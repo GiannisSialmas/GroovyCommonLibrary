a2914652eda4b5dfca68d89b495baefb6eb35f6e
Giannis Sialmas <giannissialmas@gmail.com>
2020-09-26 20:08:42 +0300

    Create configureKubectl.groovy

---
+++ vars/configureKubectl.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+// This function configures the kubectl to access your cluster.
+// You pass the cluster master ip, clusterToken, and namespace as named arguments
+// Another way to do this
+
+def call(Map args) {
+
+			sh "kubectl config set clusters.default.server ${args.clusterIp}";
+			sh "kubectl config set clusters.default.insecure-skip-tls-verify true";
+			sh "kubectl config set users.default.token ${args.clusterToken}";
+			sh "kubectl config set contexts.default.cluster default";
+			sh "kubectl config set contexts.default.user default";
+			sh "kubectl config use-context default";
+			sh "kubectl config set-context --current --namespace=${args.namespace}"
+
+}

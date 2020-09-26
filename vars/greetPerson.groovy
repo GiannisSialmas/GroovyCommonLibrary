c25d8c337fc53f3ec2b0d27aa20ef627bd00cb30
Giannis Sialmas <giannissialmas@gmail.com>
2020-09-26 18:55:47 +0300

    Update greetPerson.groovy

--- vars/greetPerson.groovy
+++ vars/greetPerson.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/env groovy
 
+//This is the default function. You can access it from the pipeline like this
+//fullName = greetPerson fname: "Giannis", lname: "Sialmas"
 def call(Map args) {
-
     fullName="${args.fname} ${args.lname}"
     println "fullName is: ${fullName}"
     return fullName
-
 }
 
+//This is the default function. You can access it from the pipeline like this
+//greeting = greetPerson.greeting fname: "Giannis", lname: "Sialmas"
 def greeting(Map args) {
-
     fullName="${args.fname} ${args.lname}"
     println "Hello ${fullName}"
     return "Hello ${fullName}"
-
 }
